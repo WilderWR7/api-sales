39a0b441e4b94af73e5901c9ebb4ab3be31456dd
WilderWR7 <wilder.mayta91@gmail.com>
2026-07-28 12:27:08 -0400

    feat: install L5-Swagger and initialize API documentation configuration

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,6 +2,25 @@
 
 namespace App\Http\Controllers;
 
+use OpenApi\Attributes as OA;
+
+#[OA\Info(
+    version: "1.0.0",
+    description: "Sales API with Laravel",
+    title: "Sales API"
+)]
+#[OA\Server(
+    url: L5_SWAGGER_CONST_HOST,
+    description: "API Server"
+)]
+#[OA\SecurityScheme(
+    securityScheme: "bearerAuth",
+    type: "http",
+    name: "Authorization",
+    in: "header",
+    scheme: "bearer",
+    bearerFormat: "JWT"
+)]
 abstract class Controller
 {
     //
