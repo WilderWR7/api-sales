dd461a2e821845f19ba8acc8104929e62ef5c482
WilderWR7 <wilder.mayta91@gmail.com>
2026-07-28 16:11:00 -0400

    feat: add ProductResource and update ProductController

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -3,16 +3,24 @@
 namespace App\Http\Controllers\Api;
 
 use App\Models\Product;
-use Illuminate\Http\Request;
+use App\Http\Resources\Api\ProductResource;
 use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 
 class ProductController extends Controller
 {
-    //
-    public function index()
+    /**
+     * Display a listing of the products.
+     *
+     * @param Request $request
+     * @return AnonymousResourceCollection
+     */
+    public function index(Request $request): AnonymousResourceCollection
     {
-        $products = Product::select('id', 'name', 'price', 'stock', 'image')->get();
+        $perPage = (int) $request->query('per_page', 10);
+        $products = Product::paginate($perPage);
 
-        return response()->json($products, 200);
+        return ProductResource::collection($products);
     }
 }
