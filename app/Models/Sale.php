de239d1937983262be0338dc48e4ed39b67118fd
WilderWR7 <wilder.mayta91@gmail.com>
2026-07-28 14:43:04 -0400

    feat(models): define eloquent relationships for sales and products

--- app/Models/Sale.php
+++ app/Models/Sale.php
@@ -12,4 +12,22 @@ class Sale extends Model
         'user_id',
         'total'
     ];
+
+    /**
+     * Get the user who made the purchase.
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo<User, Sale>
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    /**
+     * Get the details or items of the sale.
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany<SaleDetail, Sale>
+     */
+    public function details()
+    {
+        return $this->hasMany(SaleDetail::class);
+    }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -46,4 +46,13 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    /**
+     * Get the sales associated with the user.
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany<Sale, User>
+     */
+    public function sales()
+    {
+        return $this->hasMany(Sale::class);
+    }
 }
