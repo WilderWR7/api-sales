c91ef7c8a89f2f775994e675188972dc16f462fd
WilderWR7 <wilder.mayta91@gmail.com>
2026-07-28 14:08:05 -0400

    feat: implement api authentication endpoints and update user model

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\ProductController;
+use App\Http\Controllers\Api\AuthController;
 
 Route::get('/test', function () {
   return response()->json([
@@ -12,4 +13,11 @@ Route::get('/test', function () {
   ]);
 });
 
-Route::get('/products', [ProductController::class, 'index']);
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/user', [AuthController::class, 'user']);
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::get('/products', [ProductController::class, 'index']);
+});
